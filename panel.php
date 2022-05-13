5062338d01c99fae31ed5c1249afd05830e02641
Rils_s <37916737+iRoZhi@users.noreply.github.com>
2022-05-13 09:51:10 +0800

    Update panel.php

--- panel.php
+++ panel.php
@@ -14,7 +14,11 @@ include 'menu.php';
         <p>在您点击下面的按钮后，Typecho会创建一个 JSON 文件，供您保存到计算机中。</p>
         <p>导入的时间会随着数据的增加而增加，如果文章和评论数据比较多的请耐心等待提示。</p>
         <p>使用过程中如果有问题，请到 <a href="https://github.com/iRoZhi/Typecho2Halo/issues">Github</a> 提出。</p>
-
+        <p style="color:red;"> 注： 此版本仅支持halo 1.5.3版本导入</p>
+        <p>因为typecho和halo后台账号密码加密方式不一样，所以导入halo成功后登录后台</p>
+          <p>账号：admin</p>
+          <p>密码：1234567890</p>
+          <p>登录成功后建议尽快更改账号秘密</p>
         <form action="<?php $options->index('/action/Typecho2Halo?export'); ?>" method="post">
           <ul class="typecho-option typecho-option-submit" id="typecho-option-item-submit-3">
             <li>
